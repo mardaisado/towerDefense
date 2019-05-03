b0d61c2164e22e305d549f7dd6cf8f6b7e8cb655
Aurélien Héritier <mardaisado@gmail.com>
2019-05-03 16:03:47 +0200

    add Map class

--- src/ch/hevs/gdx2d/hello/Game.java
+++ src/ch/hevs/gdx2d/hello/Game.java
@@ -19,12 +19,32 @@ import ch.hevs.gdx2d.desktop.PortableApplication;
 public class Game extends RenderingScreen {
 
 	private BitmapImage imgBitmap;
+	private BitmapImage[] assets;
+	
+	private Map mapManager;
+	
+	int[][] map = {
+			{1,1,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5},
+			{1,2,3,4,5}
+	};
 
 	@Override
 	public void onInit() {
 
 		// Load a custom image (or from the lib "res/lib/icon64.png")
 		imgBitmap = new BitmapImage("data/images/hei-pi.png");
+		
+		// Load assets
+		assets = loadAssets();
+
+		// Create Map
+		mapManager = new Map(map);
 	}
 
 	@Override
@@ -37,8 +57,9 @@ public class Game extends RenderingScreen {
 		float angle = 0;
 
 		// Draw everything
-		g.drawTransformedPicture(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f, angle, 1.0f, imgBitmap);
-		g.drawStringCentered(Gdx.graphics.getHeight() * 0.8f, "Welcome to gdx2d !");
+		//g.drawTransformedPicture(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f, angle, 1.0f, imgBitmap);
+		//g.drawStringCentered(Gdx.graphics.getHeight() * 0.8f, "Welcome to gdx2d !");
+		mapManager.drawMap(g,assets);
 		//g.drawFPS();
 		//g.drawSchoolLogo();
 	}
@@ -48,6 +69,16 @@ public class Game extends RenderingScreen {
 		imgBitmap.dispose();
 		PhysicsWorld.dispose();
 		super.dispose();
+	}
+	
+	public static BitmapImage[] loadAssets() {
+		BitmapImage[] tmp = new BitmapImage[299];
+		String tmp2 = "";
+		for (int i = 0; i < tmp.length; i++) {
+			tmp2 = String.format("%03d", i);
+			tmp[i] = new BitmapImage("data/assets/PNG/Retina/towerDefense_tile" + tmp2 + ".png");
+		}
+		return tmp;
 	}
 
 //	public static void main(String[] args) {
