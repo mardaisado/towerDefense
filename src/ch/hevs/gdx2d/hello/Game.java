6369ae0d872bc0b0b0b097b4da33b08a0ee0d36c
Aurélien Héritier <mardaisado@gmail.com>
2019-05-08 10:19:05 +0200

    add tilemap support

--- src/ch/hevs/gdx2d/hello/Game.java
+++ src/ch/hevs/gdx2d/hello/Game.java
@@ -2,6 +2,10 @@ package ch.hevs.gdx2d.hello;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Interpolation;
 import ch.hevs.gdx2d.components.bitmaps.BitmapImage;
 import ch.hevs.gdx2d.components.screen_management.RenderingScreen;
@@ -29,6 +33,9 @@ public class Game extends RenderingScreen {
 			{{3,0},{3,1},{3,2},{3,3},{3,0}},
 			{{4,0},{4,1},{4,2},{4,3},{4,0}}
 	};
+	
+	TiledMap tiledMap;
+	TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void onInit() {
@@ -41,6 +48,11 @@ public class Game extends RenderingScreen {
 
 		// Create Map
 		mapManager = new Map(map);
+		
+		
+		tiledMap = new TmxMapLoader().load("data/tilemap/test1.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		
 	}
 
 	@Override
@@ -53,12 +65,19 @@ public class Game extends RenderingScreen {
 		float angle = 0;
 
 		// THE GAME
+		//g.moveCamera(1000, 1000, 1000, 1000);
+		
+		//g.zoom(4f);
+		//g.moveCamera(position.x, position.y);
+		
+		tiledMapRenderer.setView(g.getCamera());
+		tiledMapRenderer.render();
 		
 		
 		// Draw everything
 		//g.drawTransformedPicture(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f, angle, 1.0f, imgBitmap);
 		//g.drawStringCentered(Gdx.graphics.getHeight() * 0.8f, "Welcome to gdx2d !");
-		mapManager.drawMap(g,assets);
+		//mapManager.drawMap(g,assets);
 		//g.drawFPS();
 		//g.drawSchoolLogo();
 	}
