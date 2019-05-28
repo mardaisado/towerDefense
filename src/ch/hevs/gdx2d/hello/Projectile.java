4e28b69421fd1a2e20a4e7f515d26da937ac612d
Aurélien Héritier <mardaisado@gmail.com>
2019-05-28 13:49:49 +0200

    fix of projectile money

--- src/ch/hevs/gdx2d/hello/Projectile.java
+++ src/ch/hevs/gdx2d/hello/Projectile.java
@@ -55,10 +55,7 @@ public class Projectile implements DrawableObject {
 		else {
 			System.out.println("shooted ");
 			System.out.println(target.getHP());
-			if(!target.giveDamage(power))
-			{
-				Game.money.addMoneyCount(target.reward);
-			}
+			target.giveDamage(power);
 			
 			return true;
 		}
