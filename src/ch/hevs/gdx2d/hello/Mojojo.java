7f138ca40b77b718c6c68cf578ff052697912480
limace11 <41622173+limace11@users.noreply.github.com>
2019-05-28 13:41:25 +0200

    Centered the ennemie's road

--- src/ch/hevs/gdx2d/hello/Mojojo.java
+++ src/ch/hevs/gdx2d/hello/Mojojo.java
@@ -26,6 +26,12 @@ public class Mojojo extends Ennemi {
 	
 	float scale;
 	
+	int directionSave;
+	
+	boolean changeDir=false;
+	
+	Point posOld;
+	
 	public Mojojo(float scale, BitmapImage base, TiledMap map, int hp, int reward) {
 				
 		super(hp, reward);
@@ -37,9 +43,8 @@ public class Mojojo extends Ennemi {
 		System.out.println(hp);
 		
 		goStart();
-
-		changeSpeedForScale();
-
+ 
+		directionSave= (int) Utils.getTile(new Point((int) (pos.x/scale),(int) (pos.y/scale)), (TiledMapTileLayer) (map.getLayers().get(0)) ).getProperties().get("direction");		changeSpeedForScale();
 	}
 	private void changeSpeedForScale()
 	{
@@ -61,14 +66,72 @@ public class Mojojo extends Ennemi {
 			
 		if(tile.getProperties().get("direction")!=null)
 		{
-			int test = (int) tile.getProperties().get("direction");
-			
+		
 			Object test2 = tile.getProperties().get("direction");
 
 			return (int) (test2);
 		}
 		
 		return 0;
+	}
+	
+	private Point adapatPos(int dir)
+	{		
+		switch (dir) {
+		case 1:
+				return new Point( ((int) ((pos.x/scale/speed -0.5) *scale*speed) ) , pos.y);
+				
+		case 2:
+				return new Point( (pos.x ) ,(int) ((pos.y/scale -0.5) *scale));
+		
+		case 3:
+				System.out.println(new Point( ((int) ((pos.x/scale/speed +0.5) *scale*speed))  , pos.y) );
+				return new Point( ((int) ((pos.x/scale/speed +0.5) *scale*speed) ) , pos.y);
+
+		case 4:
+				return new Point( pos.x  ,(int) ((pos.y/scale/speed +0.5) *scale*speed));
+		}
+		
+		return pos;
+			
+	}
+	
+	private boolean atTheMiddle(int dir)
+	{
+		TiledMapTile currentCell = Utils.getTile(new Point((int) (pos.x/scale),(int) (pos.y/scale)), (TiledMapTileLayer) (map.getLayers().get(0)) );
+				
+		switch (dir) {
+		case 1:
+				if(Math.abs(posOld.x-pos.x) <= 0)
+				{
+					return true;
+				}
+			break;
+
+		case 2:
+				if(Math.abs(posOld.y-pos.y) <= 0)
+				{
+					return true;
+				}
+			break;
+		case 3:
+				if(Math.abs(posOld.x-pos.x) <= 0)
+				{
+					return true;
+				}
+			break;
+		case 4:
+				if(Math.abs(posOld.y-pos.y) <= 0)
+				{
+					return true;
+				}
+			break;
+		default:
+			break;
+		}
+			
+			
+		return true;
 	}
 	
 	public void goStart()
@@ -79,7 +142,7 @@ public class Mojojo extends Ennemi {
 				{					
 					if((Utils.getTile(new Point((int)((i+0.5)*64f),(int)((j+0.5)*64f)), tiledLayer).getProperties().get("start",boolean.class)) == true)
 					{	
-						pos = new Point((int)((i)*scale*64f),(int)((j+0.5)*scale*64f));
+						pos = new Point((int)((i+0)*scale*64f),(int)((j+0.5)*scale*64f));
 				  		return;
 					}
 				}
@@ -114,8 +177,6 @@ public class Mojojo extends Ennemi {
 		
 	}
 	
-	
-	
 	private TiledMapTile getTile(Point pos, int offsetX, int offsetY) {
 		try {
 			int x = (int) (pos.x / tiledLayer.getTileWidth()) + offsetX;
@@ -137,6 +198,25 @@ public class Mojojo extends Ennemi {
 		
 		return false;
 	}
+	
+	private Point getOffset(int direction)
+	{
+		switch (direction) {
+		case 1:
+				return new Point( (int) (pos.x + (0.5 * scale * 64f)) , pos.y);
+				
+		case 2:
+				return new Point( (pos.x ) ,(int) (pos.y + (0.5 * scale * 64f)));
+		
+		case 3:
+				return new Point( (int) (pos.x - (0.5 * scale * 64f)) , pos.y);
+
+		case 4:
+				return new Point( pos.x  ,(int) (pos.y - (0.5 * scale * 64f)));
+		}
+		
+		return pos;
+	}
 
 	@Override
 	public void draw(GdxGraphics g) {
@@ -150,17 +230,26 @@ public class Mojojo extends Ennemi {
 		
 		if(checkDeath())
 		{				
-			TiledMapTile currentCell = Utils.getTile(new Point((int) (pos.x/scale),(int) (pos.y/scale)), (TiledMapTileLayer) (map.getLayers().get(0)) );
-			int direction = 0;			
+			
+			int direction = 3;
+
+			TiledMapTile currentCell = Utils.getTile(new Point( (int)(getOffset(directionSave).x/scale) ,(int) (getOffset(directionSave).y/scale) ), (TiledMapTileLayer) (map.getLayers().get(0)) );
+			
 							
 			direction = getDirection(currentCell);
+			
 			progress++;
-				//System.out.println("Position update" + this );
-			goNextPosition(direction);				
+			
+			//System.out.println("Position update" + this );
+			
+			directionSave=direction;
+			
+			goNextPosition(direction);		
+			
 		}
 		else
 		{
-			pos=new Point(0,0);
+			pos= new Point(0,0);
 		}
 		
 	}
