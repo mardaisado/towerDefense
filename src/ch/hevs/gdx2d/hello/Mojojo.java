818af96559a4cb02e17640f50c282aed2d34f2fa
limace11 <41622173+limace11@users.noreply.github.com>
2019-06-05 09:41:33 +0200

    add les animaux de la ferme

--- src/ch/hevs/gdx2d/hello/Mojojo.java
+++ src/ch/hevs/gdx2d/hello/Mojojo.java
@@ -1,6 +1,7 @@
 package ch.hevs.gdx2d.hello;
 
 import java.awt.Point;
+import java.util.Random;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
@@ -21,27 +22,36 @@ public class Mojojo extends Ennemi {
 	
 	private boolean changeDir=false;
 	
-	
 	private Point posOld;
 	
 	private TiledMap map;
 	
 	private TiledMapTileLayer tiledLayer;
 	
-	
 	public Mojojo(float scale, BitmapImage base, TiledMap map, int hp, int reward) {
 				
 		super(hp, reward);
 		this.tiledLayer=(TiledMapTileLayer) map.getLayers().get(0);
-		this.base = base;
+		//this.base = base;
 		this.scale = scale;
 		this.map=map;
 		pos=goStart();
+		this.base = loadAssets()[new Random().nextInt(27)];
  
 		directionSave= (int) Utils.returnStateForInt(new Point((int) (pos.x/scale),(int) (pos.y/scale)), null, "direction", map);		
 		
 		speed = changeSpeedForScale(speed);	
 		
+	}
+	
+	public static BitmapImage[] loadAssets() {
+		BitmapImage[] tmp = new BitmapImage[27];
+		String tmp2 = "";
+		for (int i = 0; i < tmp.length; i++) {
+			tmp2 = String.format("%03d", i);
+			tmp[i] = new BitmapImage("data/assets/PNG/ANIMAL/" + i + ".png");
+		}
+		return tmp;
 	}
 	
 	/**
