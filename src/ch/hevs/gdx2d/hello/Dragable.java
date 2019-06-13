a87193ec161a491d432d7a860f3a7027f7751049
Aurélien Héritier <mardaisado@gmail.com>
2019-06-13 08:59:47 +0200

    add price of defense

--- src/ch/hevs/gdx2d/hello/Dragable.java
+++ src/ch/hevs/gdx2d/hello/Dragable.java
@@ -1,6 +1,14 @@
 package ch.hevs.gdx2d.hello;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.utils.Align;
+
 import ch.hevs.gdx2d.components.bitmaps.BitmapImage;
 import ch.hevs.gdx2d.lib.GdxGraphics;
 import ch.hevs.gdx2d.lib.interfaces.DrawableObject;
@@ -12,16 +20,27 @@ public class Dragable implements DrawableObject {
 	float width;
 	DefenseProperties properties;
 	
+	BitmapFont font;
+	
 	public Dragable(DefenseProperties properties,float posX,float posY,float width) {
 		this.properties = properties;
 		image = new BitmapImage(properties.pickImage);
 		this.posX = posX;
 		this.posY = posY;
 		this.width = width;
+		
+		FileHandle optimusF = Gdx.files.internal("data/font/Fonts/Kenney Pixel Square.ttf");
+		
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(optimusF);
+		parameter.size = generator.scaleForPixelHeight((int) (Gdx.graphics.getHeight()/50.0));
+		parameter.color = Color.WHITE;
+		font = generator.generateFont(parameter);
 	}
 	
 	public void draw(GdxGraphics g) {
 		g.drawTransformedPicture(posX,posY,0,width,width,image);
+		g.drawString(posX, posY-Gdx.graphics.getHeight()/20, properties.price+"$",font, Align.center);
 	}
 	public boolean check(int x, int y) {
 		if (x >= posX-width && x <= posX+width && y >= posY-width && y <= posY+width) {
